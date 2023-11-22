f347b3add4e523c0d35a1c719743a12f0e8cc4e7
Henry <henry@twitchyfinger.com>
2023-11-23 03:15:46 +0800

    try to rewrite all the horrible codes

--- Assets/RestaurantKit/Scripts/MainControllerExamples/MainGameController.cs
+++ Assets/RestaurantKit/Scripts/MainControllerExamples/MainGameController.cs
@@ -30,19 +30,10 @@ public class MainGameController : Singleton<MainGameController> {
 	//******************//
 	 public string gameMode{get;set;}			//game mode setting (freeplay or career)
 	 public int gameTime{get;set;}
-	 public int requiredBalance{get;set;}
-	 public int globalTrashLoss {get;set;}= 15;	//when player trashes a product or ingredient, we deduct a fixed loss from player balance.
+	
 	//*******************
 
-	//public game objects
-	public GameObject[] customers;	//list of all available customers (different patience and textures)
-
-	//public variables
-	//*****
-	//How many seats are available in this shop?
-	//These two array should always be the same size (Length)
-	public bool[] availableSeatForCustomers;
-	public Vector3[] seatPositions;
+	
 	public GameObject[] additionalItems;	//Items that can be purchased via in-game shop.
 	public GameObject endGamePlane;			//main endgame plane
 	public GameObject endGameStatus;		//gameobject which shows the texture of win/lose states
@@ -54,17 +45,7 @@ public class MainGameController : Singleton<MainGameController> {
 	 public int deliveryQueueItems{get;set;}				//number of items in delivery queue
 	 public List<int> deliveryQueueItemsContent {get;set;}= new List<int>();	//conents of delivery queue
 
-	///game timer vars
-	private string remainingTime;
-	private int seconds;
-	private int minutes;
 	
-	private int delay;					//delay between creating a new customer (smaller number leads to faster customer creation)
-	private bool  canCreateNewCustomer;	//flag to prevent double calls to functions
-
-	//Money and GameState
-	public int totalMoneyMade{get;set;}
-	//private int totalMoneyLost;
 	public bool  gameIsFinished{get;set;}	//Flag
 
 	///////////////////////////////////////
@@ -83,8 +64,6 @@ public class MainGameController : Singleton<MainGameController> {
 	public AudioClip winSfx;
 	public AudioClip loseSfx;
 
-
-	public List<CustomerController> customersInScene {private set;get;}= new List<CustomerController>();
 
 	protected override void Awake (){
 		base.Awake();
@@ -105,26 +84,11 @@ public class MainGameController : Singleton<MainGameController> {
 		deliveryQueueItems = 0;
 		deliveryQueueItemsContent.Clear();
 		
-		//customersAppeared = 0;
-		//satisfiedCutomer = 0;
-		//angryCustomer = 0;
-		totalMoneyMade = 0;
-		//totalMoneyLost = 0;
 		gameIsFinished = false;
 
 		nextButton.SetActive (false);	//only shows when we finish a level in career mode with success.
-		
-		seconds = 0;
-		minutes = 0;
-		
-		//delay = 12; //Optimal value should be between 5 (Always crowded) and 15 (Not so crowded) seconds. 
-		delay = 11;
-		canCreateNewCustomer = false;
-		
-		//set all seats as available at the start of the game. No seat is taken yet.
-		for(int i = 0; i < availableSeatForCustomers.Length; i++) {
-			availableSeatForCustomers[i] = true;
-		}
+
+	
 		
 		//check if player previously purchased these items..
 		//ShopItem index starts from 1.
@@ -147,12 +111,12 @@ public class MainGameController : Singleton<MainGameController> {
 			
 		switch(gameMode) {
 			case "FREEPLAY":
-				requiredBalance = freeplayGoalBallance;
+				
 				gameTime = 0;
 				canUseCandy = true;
 				break;
 			case "CAREER":
-				requiredBalance = PlayerPrefs.GetInt("careerGoalBallance");
+				
 				availableTime = PlayerPrefs.GetInt("careerAvailableTime");
 				//check if we are allowed to use candy in this career level
 				canUseCandy = (PlayerPrefs.GetInt("canUseCandy") == 1) ? true : false;
@@ -160,14 +124,6 @@ public class MainGameController : Singleton<MainGameController> {
 		}
 	}
 
-
-	//***************************************************************************//
-	// Starting delay. Optional.
-	//***************************************************************************//
-	IEnumerator Start (){
-		yield return new WaitForSeconds(2);
-		canCreateNewCustomer = true;
-	}
 
 
 	//***************************************************************************//
@@ -186,92 +142,19 @@ public class MainGameController : Singleton<MainGameController> {
 				manageGuiTexts();
 				StartCoroutine(checkGameWinState());
 			}
-			
-			//create a new customer if there is free seat and game is not finished yet
-			if(canCreateNewCustomer && !gameIsFinished) {
-				if(monitorAvailableSeats() != 0) {
-					createCustomer( freeSeatIndex[Random.Range(0, freeSeatIndex.Count)] );
-				} else {
-					//print("No free seat is available!");
-				}
-			}
-		}
-		
 
-	//***************************************************************************//
-	// New Customer creation routine.
-	//***************************************************************************//
-	void createCustomer ( int _seatIndex  ){
-
-		//set flag to prevent double calls 
-		canCreateNewCustomer = false;
-		StartCoroutine(reactiveCustomerCreation());
-		
-		//which customer?
-		var customerPicked = customers[Random.Range(0, customers.Length)];
-		
-		//which seat
-		Vector3 seat = seatPositions[_seatIndex];
-		//mark the seat as taken
-		
-		
-		//create customer
-		int offset = -11;
-		GameObject newCustomer = Instantiate(customerPicked, new Vector3(offset, 0.8f, 0.2f), Quaternion.Euler(0, 180, 0)) as GameObject;
-		var customerController = newCustomer.GetComponent<CustomerController>();
-		//any post creation special Attributes?
-		customerController.mySeat = _seatIndex;
-		//set customer's destination
-		customerController.destination = seat;
-
-		availableSeatForCustomers[customerController.mySeat] = false;
-		customersInScene.Add(customerController);
 	}
-
-	public void OnCustomerLeave(CustomerController customer)
-	{
-		availableSeatForCustomers[customer.mySeat] = true;
-		customersInScene.Remove(customer);
-	}
-
-
-	//***************************************************************************//
-	// customer creation is active again
-	//***************************************************************************//
-	IEnumerator reactiveCustomerCreation (){
-		yield return new WaitForSeconds(delay);
-		canCreateNewCustomer = true;
-		yield break;
-	}
-
-
-	//***************************************************************************//
-	// check if there is any free seat for customers and if true, return their index(s)
-	//***************************************************************************//
-	private List<int> freeSeatIndex = new List<int>();
-	int monitorAvailableSeats (){
-		freeSeatIndex = new List<int>();
-		for(int i = 0; i < availableSeatForCustomers.Length; i++) {
-			if(availableSeatForCustomers[i] == true)
-				freeSeatIndex.Add(i);
-		}
 		
-		//debug
-		//print("Available seats: " + freeSeatIndex);
-		
-		if(freeSeatIndex.Count > 0)
-			return -1;
-		else
-			return 0;
-	}
+
+
 
 
 	//***************************************************************************//
 	// GUI text management
 	//***************************************************************************//
 	void manageGuiTexts (){
-		moneyText.GetComponent<TextMesh>().text = "$" + totalMoneyMade.ToString();
-		missionText.GetComponent<TextMesh>().text = "$" + requiredBalance.ToString();
+		moneyText.GetComponent<TextMesh>().text = "$" + RestaurantManager.Instance.totalCoinEarned.ToString();
+		missionText.GetComponent<TextMesh>().text = "$" + RestaurantManager.Instance.coinEarnTarget.ToString();
 	}
 
 		
@@ -286,17 +169,17 @@ public class MainGameController : Singleton<MainGameController> {
 		if(gameMode == "FREEPLAY") {
 
 			gameTime = (int)Time.timeSinceLevelLoad;
-			seconds = Mathf.CeilToInt(Time.timeSinceLevelLoad) % 60;
-			minutes = Mathf.CeilToInt(Time.timeSinceLevelLoad) / 60; 
-			remainingTime = string.Format("{0:00} : {1:00}", minutes, seconds); 
+			var seconds = Mathf.CeilToInt(Time.timeSinceLevelLoad) % 60;
+			var minutes = Mathf.CeilToInt(Time.timeSinceLevelLoad) / 60; 
+			var remainingTime = string.Format("{0:00} : {1:00}", minutes, seconds); 
 			timeText.GetComponent<TextMesh>().text = remainingTime.ToString();
 			
 		} else if(gameMode == "CAREER") {
 		
 			gameTime = (int)(availableTime - Time.timeSinceLevelLoad);
-			seconds = Mathf.CeilToInt(availableTime - Time.timeSinceLevelLoad) % 60;
-			minutes = Mathf.CeilToInt(availableTime - Time.timeSinceLevelLoad) / 60; 
-			remainingTime = string.Format("{0:00} : {1:00}", minutes, seconds); 
+			var seconds = Mathf.CeilToInt(availableTime - Time.timeSinceLevelLoad) % 60;
+			var minutes = Mathf.CeilToInt(availableTime - Time.timeSinceLevelLoad) / 60; 
+			var remainingTime = string.Format("{0:00} : {1:00}", minutes, seconds); 
 			timeText.GetComponent<TextMesh>().text = remainingTime.ToString();
 		}
 
@@ -319,7 +202,7 @@ public class MainGameController : Singleton<MainGameController> {
 		if(gameIsFinished)
 			yield break;
 
-		if(gameMode == "CAREER" && gameTime <= 0 && totalMoneyMade < requiredBalance) {
+		if(gameMode == "CAREER" && gameTime <= 0 && !RestaurantManager.Instance.coinEarnTargetReached) {
 		
 			print("Time is up! You have failed :(");	//debug the result
 			gameIsFinished = true;						//announce the new status to other classes
@@ -329,7 +212,7 @@ public class MainGameController : Singleton<MainGameController> {
 			yield return new WaitForSeconds(2.0f);
 			playNormalSfx(loseSfx);
 			
-		} else if(gameMode == "CAREER" && gameTime > 0 && totalMoneyMade >= requiredBalance) {
+		} else if(gameMode == "CAREER" && gameTime > 0 && RestaurantManager.Instance.coinEarnTargetReached) {
 			
 			//save career progress
 			saveCareerProgress();
@@ -352,7 +235,7 @@ public class MainGameController : Singleton<MainGameController> {
 			//show next level button
 			nextButton.SetActive (true);
 			
-		} else if(gameMode == "FREEPLAY" && totalMoneyMade >= requiredBalance) {
+		} else if(gameMode == "FREEPLAY" && RestaurantManager.Instance.coinEarnTargetReached) {
 		
 			print("Wow, You beat the goal in freeplay mode. But You can continue... :)");
 			playNormalSfx(winSfx);
